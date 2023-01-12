63ed9e612cc2623fbe170775569ce98bd838694b
Vitor Veloso <vitor.asv@gmail.com>
2023-01-12 16:27:39 -0300

    Corrigindo o foreach

--- src/main.js
+++ src/main.js
@@ -3,17 +3,26 @@ import './style.css'
 // data.rates = [ [usd, 5] , [brl, 1] ]
 // el = [usd, 5]
 
+// if data.base === EUR {
+//  if moeda !== EUR {
+//   ERRO
+//  }
+// }
+
 function apiFetch(moeda) {
   fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
     .then(response => response.json())
-    .then(data => Object.entries(data.rates).forEach((cambio) => {
-      const cambioElement = document.querySelector('.cambio-container');
-      const clone = cambioElement.cloneNode(true);
-      clone.style.display = 'block';
-      clone.querySelector('.nome-moeda').innerHTML = cambio[0];
-      clone.querySelector('.valor-moeda').innerHTML = cambio[1];
-      document.querySelector('.moedas-container').appendChild(clone);
-    }));
+    .then(data => {
+      
+        Object.entries(data.rates).forEach((cambio) => {
+        const cambioElement = document.querySelector('.cambio-container');
+        const clone = cambioElement.cloneNode(true);
+        clone.style.display = 'block';
+        clone.querySelector('.nome-moeda').innerHTML = cambio[0];
+        clone.querySelector('.valor-moeda').innerHTML = cambio[1];
+        document.querySelector('.moedas-container').appendChild(clone);
+      });   
+    });
 }
 
 const btnSearch = document.querySelector('#btn-pesquisar');
