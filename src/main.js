cef2a886902ead2ee87a1bc227b8dca29792d0cf
herbertho <herbertho@outlook.com.br>
2023-01-12 16:44:45 -0300

    Alertas de erro

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './style.css'
+import Swal from 'sweetalert2'
 
 // data.rates = [ [usd, 5] , [brl, 1] ]
 // el = [usd, 5]
@@ -13,7 +14,15 @@ function apiFetch(moeda) {
   fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
     .then(response => response.json())
     .then(data => {
-      
+        if (data.base === "EUR" && moeda !== "EUR"){
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Moeda não existente!',
+            background: '#282c33',
+            color: 'white'
+          })
+        }else{
         Object.entries(data.rates).forEach((cambio) => {
         const cambioElement = document.querySelector('.cambio-container');
         const clone = cambioElement.cloneNode(true);
@@ -21,7 +30,8 @@ function apiFetch(moeda) {
         clone.querySelector('.nome-moeda').innerHTML = cambio[0];
         clone.querySelector('.valor-moeda').innerHTML = cambio[1];
         document.querySelector('.moedas-container').appendChild(clone);
-      });   
+      });
+    }   
     });
 }
 
@@ -30,5 +40,15 @@ const btnSearch = document.querySelector('#btn-pesquisar');
 btnSearch.addEventListener('click', () => {
   document.querySelector('.moedas-container').innerHTML = '';
   const moeda = document.querySelector('#input-moeda').value;
-  apiFetch(moeda);
+  if(moeda === ''){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Você precisa passar uma moeda!',
+      background: '#282c33',
+      color: 'white'
+    })
+  } else {
+    apiFetch(moeda);
+  }
 });
