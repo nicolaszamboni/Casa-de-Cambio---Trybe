b8a07126229c535fbc10043cfe88b246681100e1
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-01-12 18:20:49 -0300

    Projeto finalizado

--- src/main.js
+++ src/main.js
@@ -1,56 +1,47 @@
-import './style.css'
-import Swal from 'sweetalert2'
-
-// data.rates = [ [usd, 5] , [brl, 1] ]
-// el = [usd, 5]
-
-// if data.base === EUR {
-//  if moeda !== EUR {
-//   ERRO
-//  }
-// }
+import "./style.css";
+import Swal from "sweetalert2";
 
 function apiFetch(moeda) {
   fetch(`https://api.exchangerate.host/latest?base=${moeda}`)
-    .then(response => response.json())
-    .then(data => {
-        if (data.base === "EUR" && moeda !== "EUR"){
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Moeda não existente!',
-            background: '#282c33',
-            color: 'white'
-          })
-        }else {
-        const span = document.createElement('span');
-        span.innerHTML = `Valores referentes a 1 ${moeda.toUpperCase()}`
-        document.querySelector('.moedas-container').appendChild(span);
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.base === "EUR" && moeda !== "EUR") {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Moeda não existente!",
+          background: "#282c33",
+          color: "white",
+        });
+      } else {
+        const span = document.createElement("span");
+        span.innerHTML = `Valores referentes a 1 ${moeda.toUpperCase()}`;
+        document.querySelector(".moedas-container").appendChild(span);
         Object.entries(data.rates).forEach((cambio) => {
-        const cambioElement = document.querySelector('.cambio-container');
-        const clone = cambioElement.cloneNode(true);
-        clone.style.display = 'flex';
-        clone.querySelector('.nome-moeda').innerHTML = cambio[0];
-        clone.querySelector('.valor-moeda').innerHTML = cambio[1].toFixed(3);
-        document.querySelector('.moedas-container').appendChild(clone);
-      });
-    }   
+          const cambioElement = document.querySelector(".cambio-container");
+          const clone = cambioElement.cloneNode(true);
+          clone.style.display = "flex";
+          clone.querySelector(".nome-moeda").innerHTML = cambio[0];
+          clone.querySelector(".valor-moeda").innerHTML = cambio[1].toFixed(3);
+          document.querySelector(".moedas-container").appendChild(clone);
+        });
+      }
     });
 }
 
-const btnSearch = document.querySelector('#btn-pesquisar');
+const btnSearch = document.querySelector("#btn-pesquisar");
 
-btnSearch.addEventListener('click', () => {
-  document.querySelector('.moedas-container').innerHTML = '';
-  const moeda = document.querySelector('#input-moeda').value;
-  if(moeda === ''){
+btnSearch.addEventListener("click", () => {
+  document.querySelector(".moedas-container").innerHTML = "";
+  const moeda = document.querySelector("#input-moeda").value;
+  if (moeda === "") {
     Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Você precisa passar uma moeda!',
-      background: '#282c33',
-      color: 'white'
-    })
+      icon: "error",
+      title: "Oops...",
+      text: "Você precisa passar uma moeda!",
+      background: "#282c33",
+      color: "white",
+    });
   } else {
     apiFetch(moeda);
   }
